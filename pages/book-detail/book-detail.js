8dd5923938db1aac85dff15671a3545363481070
mzalbedo <1320545387@qq.com>
2018-11-02 18:27:47 +0800

    2018-11-02

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,5 +1,12 @@
-import {BookModel} from '../../models/book.js'
+import {
+  BookModel
+} from '../../models/book.js'
+import {
+  LikeModel
+} from '../../models/like.js'
 const bookModel = new BookModel()
+const likeModel = new LikeModel()
+
 
 Page({
 
@@ -7,92 +14,110 @@ Page({
    * 页面的初始数据
    */
   data: {
-    comments:[],
-    book:null,
-    likeStatus:false,
-    likeCount:0
+    comments: [],
+    book: null,
+    likeStatus: false,
+    likeCount: 0,
+    posting:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     const bid = options.bid
     // console.log(bid)
     const detail = bookModel.getDetail(bid)
     const comments = bookModel.getComments(bid)
     const likeStatus = bookModel.getLikeStatus(bid)
-    
+
     //从detail中把数据取出来，并存到data中
-    detail.then(res=>{
+    detail.then(res => {
       console.log(res)
       this.setData({
-        book:res
+        book: res
       })
     })
 
     comments.then(res => {
       console.log(res)
       this.setData({
-        comments:res.comments
+        comments: res.comments
       })
     })
 
     likeStatus.then(res => {
       console.log(res)
       this.setData({
-        likeStatus:res.like_status,
-        likeCount:res.fav_nums
+        likeStatus: res.like_status,
+        likeCount: res.fav_nums
       })
+    })
+  },
+
+  onLike: function(event) {
+    const like_or_cancel = event.detail.behavior
+    likeModel.like(like_or_cancel, this.data.book.id, 400)
+  },
+
+  onFakePost:function(event){
+    this.setData({
+      posting:true
+    })
+  },
+
+  onCancel:function(event){
+    this.setData({
+      posting: false
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
