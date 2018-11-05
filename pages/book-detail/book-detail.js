4ee9c13159c1bd88effb51b3886216d529d18fc8
mzalbedo <1320545387@qq.com>
2018-11-05 19:43:24 +0800

    2018-11-05

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -25,29 +25,32 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+
+    wx.shouLoading()  //显示加载状态
+
     const bid = options.bid
     // console.log(bid)
     const detail = bookModel.getDetail(bid)
     const comments = bookModel.getComments(bid)
     const likeStatus = bookModel.getLikeStatus(bid)
-
+    // console.log(comments)
     //从detail中把数据取出来，并存到data中
     detail.then(res => {
-      console.log(res)
+      // console.log(res)
       this.setData({
         book: res
       })
     })
 
     comments.then(res => {
-      console.log(res)
+      // console.log(res)
       this.setData({
         comments: res.comments
       })
     })
 
     likeStatus.then(res => {
-      console.log(res)
+      // console.log(res)
       this.setData({
         likeStatus: res.like_status,
         likeCount: res.fav_nums
@@ -57,7 +60,7 @@ Page({
 
   onLike: function(event) {
     const like_or_cancel = event.detail.behavior
-    likeModel.like(like_or_cancel, this.data.book.id, 400)
+    likeModel.like(like_or_cancel, this.data.book.id, 400)  //400代表书籍
   },
 
   onFakePost:function(event){
@@ -70,6 +73,42 @@ Page({
     this.setData({
       posting: false
     })
+  },
+
+  onPost(event){    
+    // const comment = event.detail.text         //用户选择的短评
+    // const commentInput = event.detail.value   //获取用户在input中输入的短评
+    const comment = event.detail.text || event.detail.value
+
+    if (!comment) {
+      return
+    }
+
+    if (comment.length > 12) {
+      wx.showToast({
+        title: '短评最多12个字',
+        icon: 'none'
+      })
+      return
+    }
+
+    bookModel.postComment(this.data.book.id, comment)
+      .then(res => {
+        wx.showToast({
+          title: '+ 1',
+          icon: "none"
+        })
+
+        this.data.comments.unshift({   //添加到数组的首位 unshift是内置的
+          content: comment,
+          nums: 1
+        })
+
+        this.setData({
+          comments: this.data.comments,
+          posting: false
+        })
+      })
   },
 
   /**
